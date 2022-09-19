6ffbdd72862e3362da11abeb847d483aebc9d77b
cloudhao1999 <cyh19991002@gmail.com>
2022-09-19 09:52:52 +0800

    feat: 更新关于界面内容

--- src/views/About.vue
+++ src/views/About.vue
@@ -3,7 +3,20 @@
 
 <template>
     <div>
-        <span class="text-xl font-bold">关于</span>
+        <p class="text-xl font-bold">关于本项目</p>
+        <p class="mt-1">
+            使用Vue.js、Vite.js、Electron技术构建的界面化Subversion提取工具 🔧 ，支持选择改动的文件，生成改动列表记录并组织成文件夹，方便查看和提取。
+        </p>
+        <p class="text-xl font-bold mt-5">版本</p>
+        <p class="mt-1">
+            1.0.0
+        </p>
+        <p class="text-xl font-bold mt-5">贡献者</p>
+        <p class="mt-1">
+            <span>Made by</span>
+            <a class="pl-2 text-blue-500 font-bold" href="https://github.com/cloudhao1999">cloudhao1999</a>、
+            <a class="text-blue-500 font-bold" href="https://github.com/Cathy-wei">Cathy-wei</a>
+        </p>
     </div>
 </template>
 
